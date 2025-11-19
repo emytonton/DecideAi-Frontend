48e43514a05790b255354699f307aa93961ff47b
Victor Veras <victorveras766@gmail.com>
2025-11-19 13:31:00 -0300

    refactor: ajuste para uso do navgraph

--- app/src/main/java/com/example/decideai_front/MainActivity.kt
+++ app/src/main/java/com/example/decideai_front/MainActivity.kt
@@ -6,8 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.decideai_front.ui.NavGraph
 import com.example.decideai_front.ui.features.LoginScreen
 import com.example.decideai_front.ui.theme.DecideAiFrontTheme
 
@@ -17,12 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DecideAiFrontTheme {
-                // O Scaffold é a estrutura base. Vamos colocar sua tela dentro dele.
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    // Chamamos sua LoginScreen aqui passando o padding para evitar sobrepor as barras do sistema
-                    LoginScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface (color = MaterialTheme.colorScheme.background){
+                    NavGraph()
                 }
             }
         }
